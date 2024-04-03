6eefe837410389c19f0db1fe18b9773dae0c22de
malekradhouane <malek.radhouen@ifollow.fr>
2024-04-03 23:19:29 +0200

    feat: add changelog to message

---
+++ changelog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run your changelog generator script and save the output to a file
+changelog-generator-script > changelog.txt
+
+# Read the changelog content from the file
+CHANGELOG_CONTENT=$(cat changelog.txt)
+
+# Set the message template with the changelog content
+MESSAGE_TEMPLATE="iTower $1 is out!\n\nChangelog:\n$CHANGELOG_CONTENT"
+
+# Output the message template
+echo "$MESSAGE_TEMPLATE"
